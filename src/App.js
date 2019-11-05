2ab391d208ef6b2da6dccd778c752ab9ce05e810
Ybrayym <ybrayymjan90@gmail.com>
2019-11-05 16:27:00 -0500

    not done

--- src/App.js
+++ src/App.js
@@ -4,8 +4,43 @@ import todosList from "./todos.json";
 
 class App extends Component {
   state = {
-    todos: todosList
+    todos: todosList,
+    value: ""
   };
+  // event handlers = inside we are using this.setState
+  handleDelete = (event, todoIdToDelete) => {
+
+    // identify what we want to change in state
+    // create a copy of state and modify it
+    const newTodoList = this.state.todos.filter(
+      todo => todo.id !== todoIdToDelete
+    );
+    // overwrite the old state with new state
+    this.setState({ todos: newTodoList });
+  }
+
+  handleCreate = (event) => {
+    // implement me!
+    // event.key
+    if (event.key === "Enter") {
+      // add the todo
+      // this.state.value
+      const newTodoList = this.state.todos.slice();
+      newTodoList.push({
+        userId: 1,
+        id: Math.floor(Math.random() * 100000000),
+        title: this.state.value,
+        completed: false
+      })
+      this.setState({ todos: newTodoList })
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({ value: event.target.value })
+  };
+
+
   render() {
     return (
       <section className="todoapp">
@@ -15,9 +50,12 @@ class App extends Component {
             className="new-todo"
             placeholder="What needs to be done?"
             autofocus
+            onKeyDown={this.handleCreate}
+            onChange={this.handleChange}
+            value={this.state.value}
           />
         </header>
-        <TodoList todos={this.state.todos} />
+        <TodoList handleDelete={this.handleDelete} todos={this.state.todos} />
         <footer className="footer">
           <span className="todo-count">
             <strong>0</strong> item(s) left
@@ -30,6 +68,7 @@ class App extends Component {
 }
 
 class TodoItem extends Component {
+  // this.props.handleDelete
   render() {
     return (
       <li className={this.props.completed ? "completed" : ""}>
@@ -40,7 +79,9 @@ class TodoItem extends Component {
             checked={this.props.completed}
           />
           <label>{this.props.title}</label>
-          <button className="destroy" />
+          <button className="destroy"
+            onClick={this.props.handleDelete}
+          />
         </div>
       </li>
     );
@@ -48,12 +89,17 @@ class TodoItem extends Component {
 }
 
 class TodoList extends Component {
+  // this.props.handleDelete
   render() {
     return (
       <section className="main">
         <ul className="todo-list">
           {this.props.todos.map(todo => (
-            <TodoItem title={todo.title} completed={todo.completed} />
+            <TodoItem
+              handleDelete={event => this.props.handleDelete(event, todo.id)}
+              title={todo.title}
+              completed={todo.completed}
+            />
           ))}
         </ul>
       </section>
