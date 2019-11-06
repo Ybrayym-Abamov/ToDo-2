24cd68edf8047dd0bcf485e2b7a16fd0b5bb49e8
Ybrayym <ybrayymjan90@gmail.com>
2019-11-06 13:57:44 -0500

    Done With Part 1

--- src/App.js
+++ src/App.js
@@ -9,15 +9,14 @@ class App extends Component {
   };
   // event handlers = inside we are using this.setState
   handleDelete = (event, todoIdToDelete) => {
-
     // identify what we want to change in state
     // create a copy of state and modify it
     const newTodoList = this.state.todos.filter(
-      todo => todo.id !== todoIdToDelete
+      todo => todo.id !== todoIdToDelete  // generate true/false
     );
     // overwrite the old state with new state
     this.setState({ todos: newTodoList });
-  }
+  };
 
   handleCreate = (event) => {
     // implement me!
@@ -32,12 +31,44 @@ class App extends Component {
         title: this.state.value,
         completed: false
       })
-      this.setState({ todos: newTodoList })
+      this.setState({ todos: newTodoList, value: "" })
     }
   }
 
+  handleToggle = todoIdToToggle => {
+    // immubility pattern
+
+    // modify the copy
+    // map
+    // 1. create a new array
+    // 2. can specify how you want to modify each item
+    const newTodoList = this.state.todos.map(todo => {
+      if (todo.id === todoIdToToggle) {
+        // modify this todo
+        // toggle its completed value 
+        // create a copy of the todo to modify
+        const newTodo = { ...todo }
+        // modify the copy 
+        newTodo.completed = !newTodo.completed;
+        // voerwrite the original with the copy
+        return newTodo;
+      }
+      // return the todo
+      return todo;
+    });
+    // overwrite the old state with new state
+    this.setState({ todos: newTodoList });
+  };
+
   handleChange = (event) => {
     this.setState({ value: event.target.value })
+  };
+
+  handleClear = (event) => {
+    const newTodoList = this.state.todos.filter(
+      todo => todo.completed === false
+    );
+    this.setState({ todos: newTodoList });
   };
 
 
@@ -49,18 +80,18 @@ class App extends Component {
           <input
             className="new-todo"
             placeholder="What needs to be done?"
-            autofocus
+            autoFocus
             onKeyDown={this.handleCreate}
             onChange={this.handleChange}
             value={this.state.value}
           />
         </header>
-        <TodoList handleDelete={this.handleDelete} todos={this.state.todos} />
+        <TodoList handleClear={this.handleClear} handleToggle={this.handleToggle} handleDelete={this.handleDelete} todos={this.state.todos} />
         <footer className="footer">
           <span className="todo-count">
             <strong>0</strong> item(s) left
           </span>
-          <button className="clear-completed">Clear completed</button>
+          <button onClick={this.handleClear} className="clear-completed">Clear completed</button>
         </footer>
       </section>
     );
@@ -69,6 +100,7 @@ class App extends Component {
 
 class TodoItem extends Component {
   // this.props.handleDelete
+  // this.props.handleToggle
   render() {
     return (
       <li className={this.props.completed ? "completed" : ""}>
@@ -77,6 +109,7 @@ class TodoItem extends Component {
             className="toggle"
             type="checkbox"
             checked={this.props.completed}
+            onChange={this.props.handleToggle}
           />
           <label>{this.props.title}</label>
           <button className="destroy"
@@ -90,12 +123,15 @@ class TodoItem extends Component {
 
 class TodoList extends Component {
   // this.props.handleDelete
+  // this.props.handleToggle
   render() {
     return (
       <section className="main">
         <ul className="todo-list">
           {this.props.todos.map(todo => (
             <TodoItem
+              key={todo.id}
+              handleToggle={event => this.props.handleToggle(todo.id)}
               handleDelete={event => this.props.handleDelete(event, todo.id)}
               title={todo.title}
               completed={todo.completed}
